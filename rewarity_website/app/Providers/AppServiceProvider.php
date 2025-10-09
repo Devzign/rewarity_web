322133e0b10540669cc83d6a3819b79ff5679407
Amit Sinha <amitsinhadeveloper@gmail.com>
2025-10-09 14:35:27 +0530

    Added Defualt login

--- rewarity_website/app/Providers/AppServiceProvider.php
+++ rewarity_website/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Support\DefaultAdmin;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $shouldEnsureDefaultAdmin = filter_var(
+            config('admin.ensure_default_on_boot', true),
+            FILTER_VALIDATE_BOOLEAN
+        );
+
+        if ($shouldEnsureDefaultAdmin) {
+            DefaultAdmin::ensure();
+        }
     }
 }
